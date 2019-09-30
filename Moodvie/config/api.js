2b81424e5fbefe1a58f5048970b1809db7641402
jmartinez <jcmartinez1925@gmail.com>
2019-09-30 14:00:53 -0700

    set up url api

--- Moodvie/config/api.js
+++ Moodvie/config/api.js
@@ -3,3 +3,26 @@ var keys = require('./keys.js');
 
 var guidebox = (keys.guidebox);
 
+// Function to show poster
+function showPoster() {
+    var movieId = ''
+    $.ajax({
+        query: 'http://api-public.guidebox.com/v2/movies/' + movieId + '/images?api_key=' + 
+            guidebox + '&filter=posters',
+        method: 'GET'
+    }).then(function(response){
+        console.log(response);
+    });
+}
+
+// Function to show viewing sources
+function viewSources() {
+    $.ajax({
+        query: 'http://api-public.guidebox.com/v2/movies?api_key=' + guidebox + '&sources=free,subscription',
+        method: 'GET'
+    }).then(function(responses) {
+        console.log(responses);
+    });
+}
+
+module.exports(client);
