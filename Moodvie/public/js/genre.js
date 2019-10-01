51e8955d71fca9d9eeed74670a389c0b776fc487
Ari Virgil-Paige <avpaige@gmail.com>
2019-09-30 18:14:38 -0700

    working genre searches

--- Moodvie/public/js/genre.js
+++ Moodvie/public/js/genre.js
@@ -1,43 +1,91 @@
+// import { generateKeyPair } from "crypto";
+
 $(document).ready(function() {
-$("#results").hide();
-$("#women").on("click", getBadAss);
-$("#even").on("click", getEven);
-// $("#dragon").on("click", getDragons);
-// $("#stalker").on("click", getDragons);
-// $("#chef").on("click", getChef);
-// $("#toys").on("click", getDragons);
-
-
-$("#closeBtn").on("click", closeBtn);
-
+    
+ 
 function closeBtn(){
     $("#results").hide();
+    $("pageBtns").hide();
     $("#genreDiv").show();
     $(".genreData").empty();
 }
 
-function getBadAss(){
-    $("#genreDiv").hide();
- $.get("/api/genres/Bad Ass Women", function(data) {
-        console.log(data)
-    for (var i=0; i<data.length; i++){ 
-        var newDiv = $("<div>");
-        newDiv.addClass("genreData");
-        var textDiv = $("<div id='resultsText'>");
-        $(textDiv).append(data[i].title);
-        $(newDiv).append(textDiv);
-        $(newDiv).show();
-        $("#results").append(newDiv);
-        $("#results").show();
-            };
-        })
-    };
+  var genres = [
+    {   
+      genre: "Bad Ass Women",
+      img: "../img/MOODVIEiconsandposter/MOV_8b118638_b.jpg"
+    },
+    {
+      genre: "Getting Even",
+      img: "../img/MOODVIEiconsandposter/images.jpeg"
+    },
+    {
+      genre: "Man vs. Machine",
+      img: "../img/MOODVIEiconsandposter/images.jpeg"
+    },
+    {
+      genre: "Alien Invasion",
+      img: "../img/MOODVIEiconsandposter/images.jpeg"
+    },
+    {
+      genre: "Dragon's In a Supporting Role",
+      img: "../img/MOODVIEiconsandposter/images.jpeg"
+    },
+    {
+        genre: "Cinderella Complex",
+        img: "../img/MOODVIEiconsandposter/images.jpeg"
+    },
+    {
+        genre: "Toys That Come to Life",
+        img: "../img/MOODVIEiconsandposter/images.jpeg"
+    },
+    {
+        genre: "YA All Day",
+        img: "../img/MOODVIEiconsandposter/images.jpeg"
+    },
+    {
+        genre: "Chef it Up",
+        img: "../img/MOODVIEiconsandposter/images.jpeg"
+    },
+    {
+       genre: "Professional Social Media Stalkers",
+       img: "../img/MOODVIEiconsandposter/images.jpeg"
+    },
+]
 
-function getEven(){
+
+  getGenres();
+  $("#results").hide();
+  $("#pageBtns").hide();
+  $(".row").on("click", searchGenres);
+  $("#closeBtn").on("click", closeBtn);
+
+  function getGenres() {
+    for (var i = 0; i < genres.length; i++) {
+      var newDiv = $("<div>");
+      newDiv.addClass("row justify-content-between");
+      newDiv.addClass("col-4 text-center");
+      newDiv.addClass("genrePassed");
+      var img = $("<img>");
+      img.addClass("poster");
+      img.attr("img-src", genres[i].img);
+      var header = $("<p>");
+      header.addClass("text-light genreText");
+      newDiv.append(header);
+      newDiv.append(img);
+      header.text(genres[i].genre);
+      newDiv.attr("data-genre", genres[i].genre);
+      $("#genreDiv").append(newDiv);
+    //   console.log(genres[i].img, "here's the image");
+    }
+  }
+
+  function searchGenres() {
     $("#genreDiv").hide();
- $.get("/api/genres/Getting Even", function(data) {
-        console.log(data)
-    for (var i=0; i<data.length; i++){ 
+    var search = $(this).attr("data-genre");
+    $.get("/api/genres/" + search, function(data) {
+      for (var i = 0; i < data.length; i++) {
+        console.log(data);
         var newDiv = $("<div>");
         newDiv.addClass("genreData");
         var textDiv = $("<div id='resultsText'>");
@@ -46,7 +94,8 @@ function getEven(){
         $(newDiv).show();
         $("#results").append(newDiv);
         $("#results").show();
-           };
-        })
-    };
-})
+        $("#pageBtns").show();
+      }
+    });
+  }
+});
